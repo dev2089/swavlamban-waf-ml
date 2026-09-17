23341c056cefa0199305fb349cbda8cb8e05a6bd
dev2089 <devanshu2089@gmail.com>
2026-09-17 16:46:35 +0530

    Phase 6: persist rules package initializer

---
+++ waf/rules/__init__.py
@@ -0,0 +1,3 @@
+"""Phase 6 managed rule lifecycle."""
+from .lifecycle import RuleLifecycleManager, RuleRecord, RuleStatus, RuleValidation
+__all__=["RuleLifecycleManager","RuleRecord","RuleStatus","RuleValidation"]
